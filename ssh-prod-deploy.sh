efc76ac03562be66042733ea6db149effde5b0c1
tanvir <tanvirrabbi0@gmail.com>
2021-02-13 23:07:29 +0100

    project dir path

--- ssh-prod-deploy.sh
+++ ssh-prod-deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-ssh -i "<PATH_TO_PEM_FILE>/MY_KEY.pem" USERNAME@SERVER_IP_ADDRESS "bash -s" << EOF
+ssh -i "<PATH_TO_PEM_FILE>/<MY_KEY>.pem" USERNAME@SERVER_IP_ADDRESS "bash -s" << EOF
     # here you just type all your commmands, as you can see, i.e.
-    cd /var/www/html/<PROJECT_DIR>;
+    cd <PATH_TO_PROJECT_DIR>/<PROJECT_DIR>;
     sudo git pull https://<BITBUCKET_USERNAME>:<BITBUCKET_PASSWORD>@bitbucket.org/<PATH_TO_REPO>/<PROJECT>.git master;
 EOF
